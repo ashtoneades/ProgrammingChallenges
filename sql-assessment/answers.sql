831d7bdc43695afa4cff51b0c4bacb5d096745e9
ashtoneades <146470353+ashtoneades@users.noreply.github.com>
2023-09-29 15:15:28 -0500

    Update answers.sql

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -29,6 +29,23 @@ GROUP BY w.state;
 #GA generated the most conversions for this campaign
 
 #Question 5
+SELECT c.name AS "Campaign Name",
+    (
+        0.2 * w.revenue +
+        0.2 * m.conversions +
+        0.2 * m.clicks -
+        0.2 * m.cost +
+        0.2 * m.impressions
+    ) AS effectiveness_score
+FROM campaign_info AS c
+INNER JOIN website_revenue AS w ON c.id = w.campaign_id
+INNER JOIN marketing_performance AS m ON c.id = m.campaign_id
+GROUP BY c.name
+ORDER BY effectiveness_score DESC
+LIMIT 1;
+#I created this query to look at which campaign was the most effective. I decided to weigh revenue, conversions, 
+  clicks, cost, and impressions all equally to create an "effectiveness score". In my opinion, campaign 4 was the most
+  effective. The company could change the weights of each variable based on what metrics they want to prioritize.
 
 #Bonus Question 6
 SELECT DAYNAME(date) AS day_of_week, AVG(clicks) AS average_clicks
