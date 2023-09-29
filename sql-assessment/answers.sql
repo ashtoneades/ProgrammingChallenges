f5f41195b58b8088cadcd8a01d1859bc7e89e2d5
ashtoneades <146470353+ashtoneades@users.noreply.github.com>
2023-09-29 13:33:40 -0500

    Update answers.sql

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -6,4 +6,9 @@ SELECT state, revenue
 FROM website_revenue 
 ORDER BY revenue DESC limit 3;
 
+SELECT m.cost, m.impressions, m.clicks, c.id, c.name
+FROM marketing_performance AS m
+INNER JOIN campaign_info AS c ON c.id = m.campaign_id
+GROUP BY c.id;
+
 
